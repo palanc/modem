94a46215221adaa5f33e78ad7c948cf6cc1f12bc
plancaster <plancaster@meta.com>
2023-03-21 22:57:10 -0500

    bc scripts for real robot pushing

--- scripts/franka/bin_push/real/real_bc.sh
+++ scripts/franka/bin_push/real/real_bc.sh
@@ -17,15 +17,15 @@ test
 
 # Train bc policies for real 
 python train.py -m \
-    task=franka-FrankaPickPlaceRandomReal_v2d  \
+    task=franka-FrankaBinPushReal_v2d  \
     suite=franka \
-    exp_name=smooth_bc_1000_demos \
+    exp_name=bin_push_real_bc \
     seed=1,2,3,4,5,6,7,8,9,10 \
-    demos=1000 \
+    demos=100 \
     img_size=224 \
     lr=3e-4 \
     batch_size=256 \
     episode_length=100 \
-    camera_views=[left_cam,right_cam] \
+    camera_views=[top_cam] \
     bc_only=true \
     hydra/launcher=slurm &
